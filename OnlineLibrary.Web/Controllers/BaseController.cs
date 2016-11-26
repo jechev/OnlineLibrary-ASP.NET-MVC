37fef4eb19843cb9794098a714c066944702f580
Dimitar Zhechev <dimityr.jechev@gmail.com>
2016-11-26 22:28:42 +0200

    Clean project from unused files

---
+++ OnlineLibrary.Web/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineLibrary.Web.Controllers
+{
+    using Data;
+    using OnlineLibrary.Models;
+
+    public abstract class BaseController : Controller
+    {
+       
+        protected BaseController(OnlineLibraryDbContext data)
+        {
+            this.Data = data;
+        }
+        protected BaseController(OnlineLibraryDbContext data, User userProfile)
+            : this(data)
+        {
+            this.UserProfile = userProfile;
+        }
+
+        protected OnlineLibraryDbContext Data { get; private set; }
+
+        protected User UserProfile { get; private set; }
+
+    }
+}
--- OnlineLibrary.Web/Controllers/HomeController.cs
+++ OnlineLibrary.Web/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 
 namespace OnlineLibrary.Web.Controllers
 {
+    using Data;
+    using OnlineLibrary.Models;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -13,18 +16,5 @@ namespace OnlineLibrary.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
