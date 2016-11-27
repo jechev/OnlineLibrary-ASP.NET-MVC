3505234a7f889136e885836ea2128875f1fd7abc
Dimitar Zhechev <dimityr.jechev@gmail.com>
2016-11-27 18:30:59 +0200

    Add List Action and View.Change imageName when it's upload

--- OnlineLibrary.Web/Controllers/BookController.cs
+++ OnlineLibrary.Web/Controllers/BookController.cs
@@ -1,6 +1,8 @@
 ﻿namespace OnlineLibrary.Web.Controllers
 {
     using System;
+    using System.IO;
+    using System.Linq;
     using System.Web.Mvc;
     using Data;
     using Microsoft.AspNet.Identity;
@@ -32,27 +34,42 @@
         {
             if (ModelState.IsValid)
             {
+
                 string folderPath = "~/media/covers/";
-                bool existFolder = System.IO.Directory.Exists(Server.MapPath(folderPath));
+                bool existFolder = Directory.Exists(Server.MapPath(folderPath));
                 if (!existFolder)
                 {
-                    System.IO.Directory.CreateDirectory(Server.MapPath(folderPath));
+                    Directory.CreateDirectory(Server.MapPath(folderPath));
                 }
-                model.ImageFile.SaveAs(HttpContext.Server.MapPath(folderPath + model.ImageFile.FileName));
-
+                var timeNowToString = DateTime.Now.ToString("ddMMyyyyhhmmsstt");
+                var imageName = timeNowToString + model.ImageFile.FileName ;
+                model.ImageFile.SaveAs(HttpContext.Server.MapPath(folderPath + imageName));
                 var newBook = new Book
                 {
                     Name = model.Name,
                     CreatedOn = DateTime.Now,
                     Genre = model.Genre,
-                    ImagePath = model.ImageFile.FileName,
+                    PageCount = model.PageCount,
+                    ImagePath = imageName,
                     CreatorId = User.Identity.GetUserId()
                 };
-                Data.Books.Add(newBook);
-                Data.SaveChanges();
-                return RedirectToAction("Index", "Home");
+                this.Data.Books.Add(newBook);
+                this.Data.SaveChanges();
+                return RedirectToAction("List");
             }
             return View(model);
+        }
+        //
+        // GET: /Book/List/
+        [HttpGet]
+        public ActionResult List()
+        {
+            var books = this.Data.Books.All()
+                .OrderByDescending(b=> b.CreatedOn)
+                .Select(BookInListViewModel.Create)
+                .ToList();
+
+            return View(books);
         }
     }
 }
---
+++ OnlineLibrary.Web/Models/BookInListViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace OnlineLibrary.Web.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq.Expressions;
+    using OnlineLibrary.Models;
+
+    public class BookInListViewModel
+    {
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public string ImgPath { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedOn { get; set; }
+
+        public int PageCount { get; set; }
+
+        public static Expression<Func<Book, BookInListViewModel>> Create
+        {
+            get
+            {
+                return book => new BookInListViewModel()
+                {
+                    Name = book.Name,
+                    Genre = book.Genre.ToString(),
+                    ImgPath = book.ImagePath,
+                    CreatedOn = book.CreatedOn,
+                    PageCount = book.PageCount
+                };
+            }
+        }
+    }
+}
